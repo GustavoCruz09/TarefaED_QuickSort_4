e9991ea1d19b807202ea84d5e7c98b9e0499ba90
Gustavo da Cruz Santos <120418063+GustavoCruz09@users.noreply.github.com>
2023-05-07 21:01:24 -0300

    First Commit
    Utilizando o algoritmo QuickSort, transforme-o em projeto Java e faça um teste com a
    ordenação do vetor (69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1)

---
+++ TarefaED_QuickSort_4/src/view/Principal.java
@@ -0,0 +1,16 @@
+package view;
+
+import GustavoCrus.QuickSort.QuickSort;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int[] vt = {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1};
+		
+		QuickSort qs = new QuickSort();
+		
+		qs.quicksort(vt, 0, vt.length - 1);
+		qs.Mostra(vt);
+	}
+
+}
